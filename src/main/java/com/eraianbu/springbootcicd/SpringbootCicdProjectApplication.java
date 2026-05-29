ed56da482b0ba86f06bc38a47e941ed7c3d745c7
eraianbu-tech <projectdevops25@gmail.com>
2026-05-29 18:33:34 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/eraianbu/springbootcicd/SpringbootCicdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.eraianbu.springbootcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicdProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eraianbu/springbootcicd/SpringbootCicdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eraianbu.springbootcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
