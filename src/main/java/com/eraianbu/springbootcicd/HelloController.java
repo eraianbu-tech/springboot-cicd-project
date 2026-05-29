a84abd6d1d3fde7ac2df6d73f5d29f9a4be6de94
eraianbu-tech <projectdevops25@gmail.com>
2026-05-29 19:30:25 +0530

    Viva demo change

--- src/main/java/com/eraianbu/springbootcicd/HelloController.java
+++ src/main/java/com/eraianbu/springbootcicd/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Welcome to Spring Boot CI/CD Project - Auto Build Test";
+        return "Welcome to Spring Boot CI/CD Project - Auto Build Test 2";
     }
 }
